48a97324481494a93dff9c4a82fe411b50aa1eb0
Liuming Huang <liuminghuang@MacBook-Air.local>
2023-11-08 16:03:41 -0500

    accessing mongodb

--- test.py
+++ test.py
@@ -1,6 +1,30 @@
 import praw
 import configparser
 import redis
+from pymongo import MongoClient
+db = MongoClient('mongodb+srv://momo78932:2gc543wG3etmmXA0@momo78932.ybq4rku.mongodb.net/?retryWrites=true&w=majority').aggregation_example
+result = db.nothing.insert_many(
+        [
+        {"x": 1, "tags": ["dog", "cat"]},
+        {"x": 2, "tags": ["cat"]},
+        {"x": 2, "tags": ["mouse", "cat", "dog"]},
+        {"x": 3, "tags": []},
+    ]
+)
+
+#result.inserted_ids
+
+l = []
+mycol = db['nothing']
+for x in mycol.find():
+    l.append(x)
+print(l[0])
+
+
+
+
+
+
 
 path_to_settings = "/Users/liuminghuang/Repos/Reddit_Post_Pipeline/secrets.ini"
 
@@ -21,7 +45,8 @@ client_secret=Configs['reddit_cred']['client_secret']
 #     print(submission.id)
 #     print('****' * 2)
 
-r = redis.Redis(host='localhost', port=6379, decode_responses=True, db = 1)
-print([val for val in r.smembers('nset')])
-r.sadd('nset', 3)
-print([val for val in r.smembers('nset')])
+# r = redis.Redis(host='localhost', port=6379, decode_responses=True, db = 1)
+# print([val for val in r.smembers('nset')])
+# r.sadd('nset', 3)
+# print([val for val in r.smembers('nset')])
+
