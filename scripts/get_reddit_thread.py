2ff8954832ad9df10c34b06e88ff8fbb48f65367
Liuming Huang <liuminghuang@MacBook-Air.local>
2023-11-10 17:26:58 -0500

    wrapping up mongodb_helper

--- scripts/get_reddit_thread.py
+++ scripts/get_reddit_thread.py
@@ -3,6 +3,8 @@ import configparser
 import sys
 sys.path.append('/Users/liuminghuang/Repos/Reddit_Post_Pipeline')
 from utilis.redis_helper import redis_connection
+from utilis.mongodb_helper import mongodb_connection, mongodb_cred
+from datetime import datetime
 
 path_to_settings = "/Users/liuminghuang/Repos/Reddit_Post_Pipeline/secrets.ini"
 
@@ -14,7 +16,6 @@ client_id=Configs['reddit_cred']['client_id']
 client_secret=Configs['reddit_cred']['client_secret']
 
 
-
 def get_thread(userAgentName, subredditName, numSubreddit):
     try:
         reddit = praw.Reddit(
@@ -23,18 +24,22 @@ def get_thread(userAgentName, subredditName, numSubreddit):
         user_agent=userAgentName
         )
         r = redis_connection('6379', 1, 'localhost')
+        mgdb_client = mongodb_connection(mongodb_cred, 'chatgpt_reddit_thread', 'top_posts')
         distinct_post_id = r.get_elements('distinct_reddit_post_id')
+        submission_document = {'subthread': subredditName, 'Date': datetime.now().strftime('%Y-%m-%d')}
+        submission_id_to_add = set()
+        submissions = []
         for submission in reddit.subreddit(subredditName).hot(limit=numSubreddit):
             if submission.id not in distinct_post_id:
-                # add submission title to mongodb
-
-                r.add_elements('distinct_reddit_post_id', submission.id)
-
-                print(submission.title)
-            
+                submissions.append({'title': submission.title, 'body': submission.selftext})
+                submission_id_to_add.add(submission.id)
+        submission_document['submissions'] = submissions
+        mgdb_client.add_document(submission_document)
+        for id in submission_id_to_add:
+            r.add_elements('reddit_title_id', id)
     except Exception as e:
         raise(f"Error accesing Reddit Thread {subredditName}: {e}")
-
+    
 
 if __name__ == "__main__":
     userAgentName = 'testing'
